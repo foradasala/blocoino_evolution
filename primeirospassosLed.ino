c13c84959df6173013c3ffc0c4a91bf7007bb0d5
foradasala <128630527+foradasala@users.noreply.github.com>
2023-03-23 09:22:41 -0300

    Update primeirospassosLed.ino

--- primeirospassosLed.ino
+++ primeirospassosLed.ino
@@ -1,37 +1,8 @@
-/*
-  Ledsimples Bloco.ino
-
-  Pisca um Led conectado ao Arduino.
-  
-  Criado em 8 de Dezembro de 2019
-  por www.foradasala.com.br
-  
- 
-*/
-
-
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
-// Esta é a área de parametrização do seu programa //////////
-// Bloco.ino. Altere os parâmetros indicados abaixo /////////
-// obter diferentes comportamentos em seu projeto  //////////
-/////////////////////////////////////////////////////////////
-
-
 // indique abaixo o numero do pino onde estão os leds 
 #define   PINO_DO_LED  13
 
 // indique abaixo o tempo que cada sinal deve ficar aceso (em segundos)
 #define   TEMPO_LED_EM_SEGUNDOS   5
-
-
-
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
-
-
-
-
 
 // a função de setup é executada apenas uma vez, quando o arduino é inicializado
 void setup() {
